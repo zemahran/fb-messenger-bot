2321b5fc7181d781a3e3537a3628779fba55e996
Zeyad <zemahran@gmail.com>
2020-05-06 10:18:37 +0000

    my latest commit

--- app.py
+++ app.py
@@ -67,33 +67,68 @@ def send_message(recipient_id):
 
     response = {
       "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": [{
-            "title": "Welcome to Moneyfellows!",
-            "subtitle": "How can we help you?",
-            "image_url": "https://moneyfellows.com/img/web_logo_large.png",
+      "type": "template",
+      "payload": {
+        "template_type": "list",
+        "top_element_style": "compact",
+        "elements": [
+          {
+            "title": "Classic T-Shirt Collection",
+            "subtitle": "See all our colors",
+            "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
             "buttons": [
               {
-                "type": "postback",
-                "title": "What is Moneyfellows?",
-                "payload": "DEVELOPER_DEFINED_PAYLOAD"
-              },
-              {
-                "type": "postback",
-                "title": "How do I pay?",
-                "payload": "DEVELOPER_DEFINED_PAYLOAD"
-              },
-              {
-                "type": "postback",
-                "title": "Are there any fees?",
-                "payload": "DEVELOPER_DEFINED_PAYLOAD"
+                "title": "View",
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                "messenger_extensions": true,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
               }
             ]
-          }]
-        }
+          },
+          {
+            "title": "Classic White T-Shirt",
+            "subtitle": "See all our colors",
+            "default_action": {
+              "type": "web_url",
+              "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+              "messenger_extensions": false,
+              "webview_height_ratio": "tall"
+            }
+          },
+          {
+            "title": "Classic Blue T-Shirt",
+            "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+            "subtitle": "100% Cotton, 200% Comfortable",
+            "default_action": {
+              "type": "web_url",
+              "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+              "messenger_extensions": true,
+              "webview_height_ratio": "tall",
+              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+            },
+            "buttons": [
+              {
+                "title": "Shop Now",
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                "messenger_extensions": true,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+              }
+            ]
+          }
+        ],
+         "buttons": [
+          {
+            "title": "View More",
+            "type": "postback",
+            "payload": "payload"
+          }
+        ]
       }
+    }
     }
 
     data = json.dumps({
